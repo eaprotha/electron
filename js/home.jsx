aa5aff96072ae8495160e4d5ac91ba2f03d25bc4
unknown <rothanak.eap@gmail.com>
2018-04-02 22:29:24 +0200

    Add suggestions

--- js/home.jsx
+++ js/home.jsx
@@ -8,9 +8,17 @@ class Home extends React.Component {
 
     constructor(props)  {
         super(props)
-        this.state = {user: '', firstname: '', lastname: '', headline: '', suggestions: ''};
+        this.state = {user: '', firstname: '', lastname: '', workNowName: '', workNowDescription: '', suggestions: ''};
     }
+
     componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            700
+        );
+    }
+
+    tick() {
         let self = this
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
@@ -18,12 +26,12 @@ class Home extends React.Component {
                 firebase.database().ref('ws/' + nodeid).once('value').then((snapshot) => {
                     let user = snapshot.val()
                     self.setState({
-                        // user: user
                         firstname: user.profile.firstName,
                         lastname: user.profile.lastName,
-                        suggestions:user.suggestions,
-                        headline: user.profile.headline
-                    })
+                        suggestions: user.suggestions,
+                        workNowName : user.profile.work_now.name,
+                        workNowDescription : user.profile.work_now.description
+                    });
                 })
             } else {
                 // User is signed out.
@@ -33,11 +41,27 @@ class Home extends React.Component {
     }
 
     render() {
+        let suggestionsList = ''
+        if(this.state.suggestions !== undefined) {
+            suggestionsList = Object.keys(this.state.suggestions).map((key, value) =>
+                <li>{this.state.suggestions[key].lastName} {this.state.suggestions[key].firstName}</li>
+            )
+        } else {
+            suggestionsList = 'NEXT BATCH IN ' + new Date().toLocaleTimeString()
+        }
+
+
         return (
             <div>
-                Hello {this.state.lastname} {this.state.firstname} {this.state.headline}
-            </div>
+                {this.state.lastname} <br />
+                {this.state.firstname} <br />
+                {this.state.workNowName} <br />
+                {this.state.workNowDescription} <br />
+                <ul>
+                    { suggestionsList }
+                </ul>
 
+            </div>
         );
     }
 }
